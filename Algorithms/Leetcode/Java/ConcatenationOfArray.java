db63a0f9327e2386ec24c3e9e6f98eeadeffee8d
André Rodrigues <mrnexusrider@hotmail.com>
2024-08-25 16:35:02 -0300

    LeetCode ConcatenationOfArray in java

---
+++ Algorithms/Leetcode/Java/ConcatenationOfArray.java
@@ -0,0 +1,13 @@
+public class ConcatenationOfArray {
+    public int[] getConcatenation(int[] nums) {
+        int size = nums.length;
+        int[] ans = new int[size * 2];
+
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[i];
+            ans[i + size] = nums[i];
+        }
+
+        return ans;
+    }
+}
