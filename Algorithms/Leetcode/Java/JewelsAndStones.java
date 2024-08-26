3ad3c6f9f00ed946e693dc86300c859d9a171733
André Rodrigues <mrnexusrider@hotmail.com>
2024-08-26 17:09:22 -0300

    JewelsAndStones in java

---
+++ Algorithms/Leetcode/Java/JewelsAndStones.java
@@ -0,0 +1,16 @@
+public class JewelsAndStones {
+    public int numJewelsInStones(String jewels, String stones) {
+        Set<Character> jewel = new HashSet<>();
+
+        for (int i = 0; i < jewels.length(); i++) {
+            jewel.add(jewels.charAt(i));
+        }
+        int count = 0;
+        for (int i = 0; i < stones.length(); i++) {
+            if (jewel.contains(stones.charAt(i))) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
